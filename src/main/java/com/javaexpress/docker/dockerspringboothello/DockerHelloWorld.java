be37027e0ef93383dc2617a4a4e6b5ceef4415dd
aecor <aecor@gmail.com>
2023-03-09 11:52:47 +0000

    commit for docker hello

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -11,6 +11,6 @@ public class DockerHelloWorld {
 	@GetMapping
 	public String getName() {
 		
-		return "Successfuly deploy your application by Docker";
+		return "Successfuly deploy your application by Dockerfile";
 	}
 }
